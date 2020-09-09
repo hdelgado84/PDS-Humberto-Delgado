55ac7dca36be9f7af53c88687e112adb41100b3a
Humberto Delgado R5053 <hdelgado84@yahoo.com.ar>
2020-09-09 20:35:16 -0300

    funcion seno en PDSMODULES

---
+++ pdsmodulos/SENO.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+
+@author: Humberto Delgado
+
+Descripcion: Funcion senoidal
+
+argumentos: Vmax = amplitud maxima
+            dc   = nivel de continua
+            ff   = frecuencia
+            N    = cantidad de muestras
+            Fs   = frecuencia de Muestreo (Nyquist)
+retorna = 
+
+"""
+#%%  inicialiizacion modulos
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+#import pdsmodulos as pds #carpeta contenedoras de funciones/modulos 
+
+
+
+#½½
+
+def SENO( vmax=1, dc=0, ff=1, N =100, ph=0, FS=1000.0):
+    
+    Ts=1/FS  #tiempo de muestreo
+    
+   
+   #grilla eje temporal
+
+    x1=np.linspace(0, (N-1), N).flatten()
+    
+    y1=np.sin(2*np.pi*x1*ff*Ts).flatten()
+
+    
+    
+     
+    return x1,y1
+
+
+#%%   
+     
+#x,y=SENO(N=1000)
+    
+#plt.plot(x, y, 'red' )
+    
+    
+    
+    
+   
